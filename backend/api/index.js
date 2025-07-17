f7ace337ee4378ee769dfd8621f27167c91f1566
nishat30 <nishatkhanam10prl@gmail.com>
2025-07-18 04:20:04 +0530

    solved error in backend deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // --- Middleware ---
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
