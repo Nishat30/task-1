841e3b1e2154bf9ca388db1b50e70372f95a11bf
nishat30 <nishatkhanam10prl@gmail.com>
2025-07-18 04:32:32 +0530

    solved error in backend deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require('../src/routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
